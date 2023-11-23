9db0873e22bfa3d9e13173a1ba218daa39274081
Jee <jeehyun24park@gmail.com>
2023-11-23 16:10:12 +0100

    ex00 - vector ut and doc ok

--- ex00/add_substract_scale_test.py
+++ ex00/add_substract_scale_test.py
@@ -5,6 +5,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from linear_algebra.vector import Vector
 from linear_algebra.matrix import Matrix
+from colorama import Fore
 
 class TestVector(unittest.TestCase):
     @classmethod
@@ -13,23 +14,78 @@ class TestVector(unittest.TestCase):
         print('----------------------------------------------------------------------')
 
     def setUp(self):
-        print(self.shortDescription())
+        print(Fore.YELLOW, self.shortDescription(), Fore.RESET)
+    
+    def test_utils(self):
+        """test utilitary functions
+        - get_size()
+        - __str__()
+        - reshape() 
+        """
+        test = Vector([1.0, 1.0])
+        self.assertEqual(test.__str__(), '[1.0, 1.0]')
+        self.assertEqual(test.get_size(), 2)
+        try:
+            empty = Vector([])
+        except ExceptionError as error:
+            print(f"TypeError: {error}")
+        
+        test_reshape = Vector([1, 2, 3, 4, 5, 6])
+        self.assertEqual(test_reshape.reshape(2, 3), [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+        try:
+            self.assertEqual(test_reshape.reshape(2, 2), [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+        except ValueError as error:
+            print(f"ValueError: {error}")
 
     def test_add(self):
         """test add a vector
         """
-        test = Vector()
-        self.assertEqual(test.add(), "add")
+        u = Vector([2., 3.])
+        v = Vector([5., 7.])
+        print(f"u = {u.__str__()}")
+        print(f"v = {v.__str__()}")
+        print(f"u = {u.__str__()} + {v.__str__()}")
+        self.assertEqual(u.add(v), [7.0, 10.0])
+        print(f"u = {u.__str__()}\n")
+
+        u = Vector([2.])
+        v = Vector([5., 7.])
+        print(f"u = {u.__str__()}")
+        print(f"v = {v.__str__()}")
+        print(f"u = {u.__str__()} + {v.__str__()}")
+        try:
+            self.assertEqual(u.add(v), [7.0, 10.0])
+        except ValueError as error:
+            print(f"ValueError: {error}")
         
     def test_subtract(self):
         """test substract a vector
         """
-        test = Vector()
+        u = Vector([2., 3.])
+        v = Vector([5., 7.])
+        print(f"u = {u.__str__()}")
+        print(f"v = {v.__str__()}")
+        print(f"u = {u.__str__()} + {v.__str__()}")
+        self.assertEqual(u.sub(v), [-3.0, -4.0])
+        print(f"u = {u.__str__()}\n")
+
+        u = Vector([2.])
+        v = Vector([5., 7.])
+        print(f"u = {u.__str__()}")
+        print(f"v = {v.__str__()}")
+        print(f"u = {u.__str__()} + {v.__str__()}")
+        try:
+            self.assertEqual(u.sub(v), [-3.0, -4.0])
+        except ValueError as error:
+            print(f"ValueError: {error}")
 
     def test_scale(self):
         """test multiply a vector by a scalar
         """
-        test = Vector()
+        u = Vector([2., 3.])
+        print(f"u = {u.__str__()} * 2")
+        self.assertEqual(u.scl(2.), [4.0, 6.0])
+        print(f"u = {u.__str__()}")
 
 class TestMatrix(unittest.TestCase):
     @classmethod
@@ -43,18 +99,25 @@ class TestMatrix(unittest.TestCase):
     def setUp(self):
         print(self.shortDescription())
 
-    def test_add(self):
-        """test add a matrix
+    def test_utils(self):
+        """test utilitary functions
+        - get_shape()
+        - __str__()
+        - reshape()
+        - is_square() 
         """
-        test = Matrix()
-        self.assertEqual(test.add(), "add")
+#     def test_add(self):
+#         """test add a matrix
+#         """
+#         test = Matrix()
+#         self.assertEqual(test.add(), "add")
         
-    def test_subtract(self):
-        """test substract a matrix
-        """
-        test = Matrix()
+#     def test_subtract(self):
+#         """test substract a matrix
+#         """
+#         test = Matrix()
 
-    def test_scale(self):
-        """test multiply a matrix by a scalar
-        """
-        test = Matrix()
+#     def test_scale(self):
+#         """test multiply a matrix by a scalar
+#         """
+#         test = Matrix()
--- linear_algebra/vector.py
+++ linear_algebra/vector.py
@@ -1,12 +1,93 @@
 class Vector:
-    def __init__(self):
-        print("Class Vector init")
-    
-    def add(self):
-        return("add")
-    
-    def sub(self):
-        return("substract")
 
-    def scl(self):
-        return("scale")
+    def __init__(self, data: list[float]):
+        """initialize a vector with the given data
+
+        Args:
+            data (list[float]): list of values for the vector
+        """
+        try:
+            assert len(data) != 0, "cannot init an empty vector"
+            # TODO: cast all values in float ? 
+            # self.data = [float(x) for x in data]
+            self.data = data
+        except AssertionError as e:
+            print('error:', e)
+
+    def __str__(self):
+        return str(self.data)
+
+    def add(self, v: 'linear_algebra.vector.Vector'):
+        """Adds another vector to this vector
+
+        Args:
+            v (linear_algebra.vector.Vector): the other vector to add 
+
+        Raises:
+            ValueError: if vectors dimensions don't match
+
+        Returns:
+            Vector: updated Vector
+        """
+        if len(self.data) != len(v.data):
+           raise ValueError(f"cannot add vector of size {len(self.data)} and vector of size {len(v.data)}")
+        self.data = [a + b for a, b in zip(self.data, v.data)]
+        return self.data
+    
+    def sub(self, v: 'linear_algebra.vector.Vector'):
+        """Substract this vector by another vector
+
+        Args:
+            v (linear_algebra.vector.Vector): the other vector to substract
+
+        Raises:
+            ValueError: if vectors dimensions don't match
+
+        Returns:
+            Vector: updated Vector
+        """
+        if len(self.data) != len(v.data):
+           raise ValueError(f"cannot add vector of size {len(self.data)} and vector of size {len(v.data)}")
+        self.data = [a - b for a, b in zip(self.data, v.data)]
+        return self.data
+
+    def scl(self, a: float):
+        """Scales the vector by a scalar.
+
+        Args:
+            scalar (float): the scalar value.
+
+        Returns:
+            Vector: a new vector representing the scaled vector.
+        """
+        self.data = [a * b for b in self.data]
+        return self.data
+    
+    def get_size(self):
+        """Returns size of vector
+
+        Returns:
+            int: size of the vector
+        """
+        return len(self.data)
+
+    def reshape(self, row, column):
+        """reshape vector to a row * column matrix
+
+        Args:
+            row (int): number of rows of the desired matrix
+            column (int): number of columns of the desired matrix
+
+        Raises:
+            ValueError: matrix dimension must match with vector size
+
+        Returns:
+            list[[]]: reshaped matrix
+        """
+        if row * column != len(self.data):
+           raise ValueError(f"cannot reshape array of size {len(self.data)} into shape ({row},{column})")
+        reshaped_matrix = []
+        for i in range(row):
+            row = self.data[i * column : (i + 1) * column]
+            reshaped_matrix.append(row)
+        return reshaped_matrix
