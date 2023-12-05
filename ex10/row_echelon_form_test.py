a0f4c0b5e71a79b89a453500eb71f4507b471547
Jee <jeehyun24park@gmail.com>
2023-12-05 15:26:30 +0100

    ex10: fixed already reduced matrix cases

--- ex10/row_echelon_form_test.py
+++ ex10/row_echelon_form_test.py
@@ -17,26 +17,45 @@ class TestMatrix(unittest.TestCase):
     def setUp(self):
          print(Fore.LIGHTCYAN_EX, self.shortDescription(), Fore.RESET)
     def test_row_echelon(self):
-        u = Matrix([[1., 0., 0], [0., 1, 0], [0., 0., 1]])
+        u = Matrix([[0., 0., 0.], [0., 0., 0.], [0., 0., 0.]])
+        print(f'Matrix:\n{u.__str__()}')
         res = u.row_echelon()
-        print(res.__str__(), type(res))
+        print(res.__str__())
+        self.assertEqual(res.data, [[0., 0., 0.], [0., 0., 0.], [0., 0., 0.]])
+
+        u = Matrix([[1., 0., 0.], [0., 0., 0.], [0., 0., 0.]])
+        print(f'Matrix:\n{u.__str__()}')
+        res = u.row_echelon()
+        print(f'Reduced Matrix:\n{res.__str__()}')
+        self.assertEqual(res.data, [[1., 0., 0.], [0., 0., 0.], [0., 0., 0.]])
+
+        u = Matrix([[1., 1., 1.], [1., 1., 1.], [1., 1., 1.]])
+        print(f'Matrix:\n{u.__str__()}')
+        res = u.row_echelon()
+        print(f'Reduced Matrix:\n{res.__str__()}')
+        self.assertEqual(res.data, [[1., 1., 1.], [0., 0., 0.], [0., 0., 0.]])
+        
+        u = Matrix([[1., 0., 0], [0., 1, 0], [0., 0., 1]])
+        print(f'Matrix:\n{u.__str__()}')
+        res = u.row_echelon()
+        print(f'Reduced Matrix:\n{res.__str__()}')
+        self.assertEqual(res.data, [[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
         
         u = Matrix([[1., 2.], [3., 4.]])
+        print(f'Matrix:\n{u.__str__()}')
         res = u.row_echelon()
-        print(res.__str__(), type(res))
+        print(f'Reduced Matrix:\n{res.__str__()}')
+        self.assertEqual(res.data, [[1., 0.], [0., 1.]])
 
         u = Matrix([[1., 2.], [2., 4.]])
+        print(f'Matrix:\n{u.__str__()}')
         res = u.row_echelon()
-        print(res.__str__(), type(res))
+        print(f'Reduced Matrix:\n{res.__str__()}')
+        self.assertEqual(res.data, [[1., 2.], [0., 0.]])
+
 
         u4 = Matrix([[8., 5., -2., 4., 28.], [4., 2.5, 20., 4., -4.], [8., 5., 1., 4., 17.]])
+        print(f'Matrix:\n{u.__str__()}')
         res = u4.row_echelon()
-        print(res.__str__(), type(res))
-        # testing find_pivot
-        # u4 = Matrix([[8., 5., -2., 4., 28.], [4., 2.5, 20., 4., -4.], [8., 5., 1., 4., 17.]])
-        # res = u4.row_echelon()
-        # print(res.__str__(), type(res))
-
-        # u4 = Matrix([[1., 0.625, -0.25, 0.5, 3.5], [0., 0., 21., 2., -18.], [0., 0., 3., 0., -11.]])
-        # res = u4.row_echelon()
-
+        print(f'Reduced Matrix:\n{res.__str__()}')
+        self.assertEqual(res.data, [[1.0, 0.625, 0.0, 0.0, -12.166666666666668], [0.0, 0.0, 1.0, 0.0, -3.6666666666666665], [-0.0, -0.0, -0.0, 1.0, 29.5]])
